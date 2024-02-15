3115dc0338b1f486cdbcd18de3bbaf73e1ee5ff9
DRroth76 <david.randall.roth97@gmail.com>
2024-02-15 13:19:09 -0600

    The app.js file is updated.

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,61 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-function chartsFunction(sample_info){
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-        // console.log("Data from samples.json:", data);
-      console.log(data)
+function chartsFunction(sample_info) {
+  d3.json(url).then((data) => {
         const numberofSamples = data.samples;
-        //console.log("Number of Samples:", numberofSamples)
 
         const selectedSample = numberofSamples.filter(sample => sample.id == sample_info);
         const firstSample = selectedSample[0];
         
-        const otuIDs = firstSample.otu_ids;
-        const otuLabels = firstSample.otu_labels;
-        const sampleValues = firstSample.sample_values;
+        const otuIDs = firstSample.otu_ids.slice(0, 10);
+        const otuLabels = firstSample.otu_labels.slice(0, 10);
+        const sampleValues = firstSample.sample_values.slice(0, 10);
 
         let trace = {
           x: sampleValues,
-          y: otuIDs,
+          y: otuIDs.map(id => `OTU ${id}`),
+          text: otuLabels,
           type: "bar",
+          orientation: 'h'
         };
-        Plotly.newPlot("bar", trace)
+        let layout = {
+          yaxis: {
+            autorange: "reversed"
+          }
+        };
+        Plotly.newPlot("bar", [trace], layout);
+        
+        
+        let traceBubble = {
+          x: firstSample.otu_ids,
+          y: firstSample.sample_values,
+          text: firstSample.otu_labels,
+          mode: 'markers',
+          marker: {
+            size: firstSample.sample_values,
+            color: firstSample.otu_ids,
+          }
+        };
 
-       // for (let i = 0; i < numberofSamples; i++){
-         //   const otuIDs = data.samples[i].otu_ids.slice(0, 10);
-          //  const sampleValues = data.samples[i].sample_values.slice(0, 10);
-           // const otuLabels = data.samples[i].otu_labels.slice(0, 10);
+        let layoutBubble = {
+          xaxis: { title: 'OTU ID' },
+          yaxis: { title: 'Sample Values' },
+          showlegend: false
+        };
 
-        //console.log("OTU IDs:", otuIDs);
-        //console.log("OTU Labels:", otuLabels);
-        //console.log("Sample Values:", sampleValues);
-        }
-)
-    //.catch(error => {
-      //  console.error("Error fetching data:", error);
-   // });
+        Plotly.newPlot("bubble", [traceBubble], layoutBubble);
+      });
+      
+      }
+        
 
-//chartsFunction(sample_info)};
-    };
+      
+function init(){
+  d3.json(url).then((data) => {
+    let sampleNames = data.names;
+    let firstname = sampleNames[0];
+
+    chartsFunction(firstname);
+});
+  }
+init();
